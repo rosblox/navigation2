faca047cdf4da6875753d57e5fd5d7d5410dedf5
Max Polzin <polzin.max@gmail.com>
2023-10-13 15:38:31 +0200

    Add CanRobotRollCondition.

---
+++ nav2_behavior_tree/plugins/condition/can_robot_roll_condition.cpp
@@ -0,0 +1,57 @@
+// Copyright (c) 2021 Joshua Wallace
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#include "nav2_behavior_tree/plugins/condition/can_robot_roll_condition.hpp"
+#include <chrono>
+#include <memory>
+#include <string>
+
+namespace nav2_behavior_tree
+{
+
+CanRobotRollCondition::CanRobotRollCondition(
+  const std::string & condition_name,
+  const BT::NodeConfiguration & conf)
+: BT::ConditionNode(condition_name, conf)
+{
+  node_ = config().blackboard->get<rclcpp::Node::SharedPtr>("node");
+  client_ = node_->create_client<slope_estimation_interfaces::srv::GetFloat32>("/slope_estimation/get_slope_estimate");
+
+  server_timeout_ = config().blackboard->template get<std::chrono::milliseconds>("server_timeout");
+  getInput<std::chrono::milliseconds>("server_timeout", server_timeout_);
+}
+
+BT::NodeStatus CanRobotRollCondition::tick()
+{
+  auto request = std::make_shared<slope_estimation_interfaces::srv::GetFloat32::Request>();
+
+  auto result = client_->async_send_request(request);
+
+  if (rclcpp::spin_until_future_complete(node_, result, server_timeout_) ==
+    rclcpp::FutureReturnCode::SUCCESS)
+  {
+    if (result.get()->data >= 5.0) {
+      return BT::NodeStatus::SUCCESS;
+    }
+  }
+  return BT::NodeStatus::FAILURE;
+}
+
+}  // namespace nav2_behavior_tree
+
+#include "behaviortree_cpp_v3/bt_factory.h"
+BT_REGISTER_NODES(factory)
+{
+  factory.registerNodeType<nav2_behavior_tree::CanRobotRollCondition>("CanRobotRoll");
+}
